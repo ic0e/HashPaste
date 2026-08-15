2951ac8b68e28764e5e32bcd2cf5ebf36a27bd72
ic0e <67431669+ic0e@users.noreply.github.com>
2026-08-15 20:18:17 +0300

    feat: added decompressText to compression.ts

--- src/engine/compression.ts
+++ src/engine/compression.ts
@@ -14,7 +14,28 @@ export async function compressText(text: string): Promise<string> {
   return bytesToBase64Url(compressedBytes);
 }
 
+export async function decompressText(hash: string): Promise<string> {
+  let base64 = hash.replace(/-/g, '+').replace(/_/g, '/');
+    while (base64.length % 4 !== 0) {
+      base64 += '=';
+    }
 
+    const binaryString = atob(base64);
+      const bytes = new Uint8Array(binaryString.length);
+      for (let i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+      }
+
+  const ds = new DecompressionStream('deflate-raw');
+  const writer = ds.writable.getWriter();
+  await writer.write(bytes.buffer as ArrayBuffer);
+  await writer.close();
+
+  const decompressedBuffer = await new Response(ds.readable).arrayBuffer();
+
+  const decoder = new TextDecoder();
+  return decoder.decode(decompressedBuffer);
+}
 
 
 // get safe URLs
